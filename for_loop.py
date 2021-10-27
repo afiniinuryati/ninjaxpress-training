83c47b5326ec42aeec23df179c3a425489def700
unknown <afinii30@gmail.com>
2021-10-27 17:56:48 +0700

    Simpan semua file Pert-3

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+nama_buah = ["apel","jeruk","mangga"]
+print(nama_buah[0])
+print(nama_buah[1])
+
+
+for buah in nama_buah:
+    print(buah)
+    print("---")
---
+++ range.py
@@ -0,0 +1,13 @@
+# x = range(3)
+# for i in x:
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+nama_anak = []
+nama_anak.append(input("Nama anak 1:"))
+nama_anak.append(input("Nama anak 2:"))
+for nama in nama_anak:
+    print("Nama anak adalah..")
+    print(nama)
